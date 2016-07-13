fff72a0136edf85f2ecd6481c90cbded0f3047bd
Satish Goda <satishgoda@live.com>
2016-07-13 21:46:40 +0800

    After using the prototype pattern
    Can't instantiate abstract class Monster with abstract methods clone
    Instance of <class '__main__.Ghost'> at 0x1ed1250: _health=100  _speed=45
    Instance of <class '__main__.Ghost'> at 0x1ed1270: _health=100  _speed=45
    Instance of <class '__main__.Ghost'> at 0x1ed12f0: _health=100  _speed=100

--- code/python/revisit/prototype/after.py
+++ code/python/revisit/prototype/after.py
@@ -9,20 +9,26 @@ class Monster(object):
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def clone(cls):
+    def clone(self):
         pass
 
 class Ghost(Monster):
-    @classmethod
-    def clone(cls):
-        return cls()
+    def __init__(self, health, speed):
+        self._health = health
+        self._speed = speed
 
-class Demon(Monster):
-    pass
+    def clone(self):
+        return self.__class__(self._health, self._speed)
 
-class Sorcerer(Monster):
-    pass
+    def __repr__(self):
+        return "Instance of {0} at {1}: _health={2}  _speed={3}".format(self.__class__, hex(id(self)), self._health, self._speed)
 
+class Spawner(object):
+    def __init__(self, monsterPrototype):
+        self._monsterPrototype = monsterPrototype
+
+    def spawnMonster(self):
+        return self._monsterPrototype.clone()
 
 if __name__ == '__main__':
     try:
@@ -30,6 +36,11 @@ if __name__ == '__main__':
     except TypeError as e:
         print e
 
-    ghost1 = Ghost()
+    ghost1 = Ghost(100, 45)
     print ghost1
     print ghost1.clone()
+
+    fastestGhostPrototype = Ghost(100, 100)
+    fastestGhostSpawner = Spawner(fastestGhostPrototype)
+
+    print fastestGhostSpawner.spawnMonster()
