42a77ca603e07ccbae805edcf88d64032345a9fa
Satish Goda <satishgoda@live.com>
2016-07-14 20:09:04 +0800

    Prototype usage example

--- code/python/revisit/Prototype/__init__.py
+++ code/python/revisit/Prototype/__init__.py
@@ -1,1 +1,37 @@
 
+"""
+http://gameprogrammingpatterns.com/prototype.html
+http://en.wikipedia.org/wiki/Prototype_pattern
+"""
+
+import before
+import after
+
+def usage():
+    """
+    import Prototype
+
+    print Prototype.usage()
+
+    try:
+        eval(Prototype.usage())
+    except SyntaxError as e:
+        pass
+
+    results = {}
+
+    exec Prototype.usage() in results
+
+    print results.keys()
+
+    :return: python code string
+    """
+    return """
+import Prototype
+
+ghost1 = Prototype.after.Ghost(100, 45)
+
+ghost1.clone()
+
+spawnedGhost = Prototype.after.Spawner(Prototype.after.Ghost(100, 45)).spawnMonster()
+    """
